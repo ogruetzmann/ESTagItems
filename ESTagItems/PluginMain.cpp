eb62b852e8c8af877f4f91635b4fb6b18b28c3c7
Olli <o.gruetzmann@gmail.com>
2016-04-14 20:30:24 +0200

    .

---
+++ ESTagItems/PluginMain.cpp
@@ -0,0 +1,12 @@
+#include "ESTagItems.h"
+
+
+void __declspec (dllexport) EuroScopePlugInInit(EuroScopePlugIn::CPlugIn **pPlugInInstance)
+{
+
+}
+
+void    __declspec (dllexport)    EuroScopePlugInExit(void)
+{
+
+}
