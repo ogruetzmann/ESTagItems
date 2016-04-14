747536707c6e219a0849d1fef9e742c8dde3dbad
Olli <o.gruetzmann@gmail.com>
2016-04-14 20:12:08 +0200

    Initial test-commit

---
+++ ESTagItems/PluginMain.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+#include <EuroScopePlugIn.h>
+
+void __declspec (dllexport) EuroScopePlugInInit(EuroScopePlugIn::CPlugIn **pPlugInInstance)
+{
+
+}
+
+void    __declspec (dllexport)    EuroScopePlugInExit(void)
+{
+
+}
